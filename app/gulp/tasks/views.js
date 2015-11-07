2fbb2943f6a2ebb69a9c6ac4c99a1ec29e7d2315
cz-Michael <zchning@gmail.com>
2015-11-07 00:27:01 -0500

    add gulp view

---
+++ app/gulp/tasks/views.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var config = require('../config');
+var gulp = require('gulp');
+var templateCache = require('gulp-angular-templatecache');
+
+// Views task
+gulp.task('views', function() {
+
+  // Put our index.html in the dist folder
+  gulp.src(config.views.index)
+    .pipe(gulp.dest(config.buildDir));
+
+  // Process any other view files from app/views
+  return gulp.src(config.views.src)
+    .pipe(templateCache({
+      standalone: true
+    }))
+    .pipe(gulp.dest(config.views.dest))
+    .pipe(browserSync.stream({ once: true }));
+
+});
